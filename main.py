ecc96e9e9e76398e3c7bb7bc15ef20cdbb338288
Abu Bakar Syed <syed.abubakar73@gmail.com>
2026-04-09 15:36:11 +0800

    able to play with computer but no winners

--- main.py
+++ main.py
@@ -14,17 +14,42 @@ def main():
     board = [[1,2,3],[4,5,6],[7,8,9]]
     computerTurn(board)
     drawBorder(board)
-    print(board)
-    
-   #user_input = int(input("enter your move number: "))
 
+    while True:
+        hasDigit = [True for row in board for col in row if type(col) == int]
+        if len(hasDigit) == 0:
+            break
+        else:
+            user_input = int(input("enter your move number: "))
+            userTurn(board,user_input)
+            computerTurn(board)
+            drawBorder(board)
 
 def computerTurn(board):
+    hasFound = False
     computer_choose = randrange(1,10)
+    print(computer_choose)
     for indexRow,row in enumerate(board):
         for indexCol,col in enumerate(row):
-            if computer_choose == col:
+            if col == "X" or col == "O":
+                continue
+            elif computer_choose == col:
                 board[indexRow][indexCol] = "X"
+                hasFound = True
+                break
+            elif hasFound == False and indexRow == len(board)-1 and indexCol == len(row)-1:
+                print("recurse")
+                return computerTurn(board)
+        if hasFound:
+            return board
+            break
+
+def userTurn(board,user_input):
+    for indexRow,row in enumerate(board):
+        for indexCol,col in enumerate(row):
+            if user_input == col:
+                board[indexRow][indexCol] = "O"
+                return board
 
 if __name__ == "__main__":
     main()
