2519b552e1e2a250390b32de2522804c8d52da72
Abu Bakar Syed <syed.abubakar73@gmail.com>
2026-04-09 17:11:57 +0800

    Completed: Contain bug where the when draw nothing happens

--- main.py
+++ main.py
@@ -17,26 +17,27 @@ def main():
 
     while True:
         hasDigit = [True for row in board for col in row if type(col) == int]
-        hasWon = checkWinner(board)
-        if len(hasDigit) == 0:
-            break
-        elif hasWon:
-            print("Someone Won!!")
-            break
-        else:
-            user_input = int(input("enter your move number: "))
-            userTurn(board,user_input)
-            computerTurn(board)
+        hasWon,userWon = checkWinner(board)
+        if hasWon:
             drawBorder(board)
+            if userWon:
+                print("You Won!!")
+            else:
+                print("Computer Won!!")
+            break
+        elif len(hasDigit) == 0:
+            break
+        user_input = int(input("enter your move number: "))
+        userTurn(board,user_input)
+        computerTurn(board)
+        drawBorder(board)
 
 def computerTurn(board):
     computer_choose = randrange(1,10)
     print(computer_choose)
     for indexRow,row in enumerate(board):
         for indexCol,col in enumerate(row):
-            if col == "X" or col == "O":
-                continue
-            elif computer_choose == col:
+            if computer_choose == col:
                 board[indexRow][indexCol] = "X"
                 return board
             elif indexRow == len(board)-1 and indexCol == len(row)-1:
@@ -53,11 +54,22 @@ def userTurn(board,user_input):
 def checkWinner(board):
     for index in range(3):
         if board[index][0] == board[index][1] and board[index][0] == board[index][2]:
-            return True
+            if board[index][0] == "O":
+                return True,True
+            return True,False
         elif board[0][index] == board[1][index] and board[0][index] == board[2][index]:
-            return True
-        return False
+            if board[0][index] == "O":
+                return True,True
+            return True,False
+        elif board[0][0] == board[1][1] and board[0][0] == board[2][2]:
+            if board[0][0] == "O":
+                return True,True
+            return True,False
+        elif board[0][2] == board[1][1] and board[0][0] == board[2][0]:
+            if board[0][2] == "O":
+                return True,True
+            return True,False
+    return False,False
 
 if __name__ == "__main__":
     main()
-    
